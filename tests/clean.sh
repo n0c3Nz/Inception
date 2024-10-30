40a11610245c36bd820e2ad3a3edee6b0a956f40
c3nz <agapitobusiness@gmail.com>
2024-10-30 14:10:03 +0100

    0.0.3

--- tests/clean.sh
+++ tests/clean.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+# Colores
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+NC="\033[0m" # Sin color
+
 image_name=img-test
 container_name=cont-test
 hostname=cueva
-docker rm -f $(docker ps -aq) >/dev/null && echo "Contenedores parados y eliminados" || echo "No había contenedores que borrar"
-docker rmi $(docker images -q) >/dev/null && echo "Imagenes eliminadas" || echo "No había imagenes que borrar"
+echo -n "---- CLEANING: "
+docker rm -f $(docker ps -aq) 2>/dev/null && echo -e "${GREEN}Contenedores eliminados${NC}" || echo -e "${RED}No había contenedores que borrar${NC}"
+docker rmi $(docker images -q) 2>/dev/null && echo -e "${GREEN}Imagenes eliminadas${NC}" || echo -e "${RED}No había imagenes que borrar${NC}"
 docker volume prune -f
-
-docker build -t $image_name . >/dev/null && echo "Imagen montada" || echo "La imagen no ha podido ser creada"
-docker run -d -v /workspaces/Inception/tests/compartido:/compartido --name $container_name --hostname $hostname $image_name && echo "Contenedor corriendo" || echo "El contenedor no ha podido desplegarse"
+echo "---- BUILDING: "
+docker build -t $image_name . && echo -e "${GREEN}Imagen montada${NC}" || echo -e "${RED}La imagen no ha podido ser creada${NC}"
+echo "---- RUNNING: "
+docker run -p 4242:4242 -d -v /workspaces/Inception/tests/compartido:/compartido --name $container_name --hostname $hostname $image_name || echo "${RED}El contenedor no ha podido desplegarse${NC}"
 
 docker exec -it $container_name /bin/bash
+
+
+# DOCKER RUN:
+# -p HOST:CONTAINER (port binding) para mapear un puerto del host al contenedor
+# -d (detach) para que el contenedor corra en segundo plano
+# -v HOST:CONTAINER (bind mount) para montar un directorio del host en el contenedor
+# --name nombre del contenedor
+# --hostname nombre del host
